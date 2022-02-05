fc816b8ce1f54a5ed5aacf445a2d82a04c88671e
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-02-05 19:37:08 +0000

    debug toolbar OK

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,23 @@
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Привет, World!!!"
+    user = {'username': 'Miguel'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+
 
 @app.route('/test')
 def tst():
-    a=10
+    a = 10
 
-    return str(5/a)+" Привет, World!!!"    
+    return str(5/a)+" Привет, World!!!"
