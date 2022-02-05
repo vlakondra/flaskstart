7416145e9a66aff9ca3f2ea6b068cb9b44cef079
vlakondra <kondrashkin.wladimir@yandex.ru>
2022-02-05 19:23:39 +0000

    debug toolBar

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.debug = True
 print('app? ', app)
+app.config['SECRET_KEY'] = '123456789'
+print(app.config)
+toolbar = DebugToolbarExtension(app)
 
 from app import routes
